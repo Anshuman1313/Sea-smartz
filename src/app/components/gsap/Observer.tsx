cc7113ac9a2ef8412c6e85bc39fc7ed0c454e17e
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2025-11-04 16:57:28 +0530

    Some more fixes  css
    Some more fixes css

---
+++ src/app/components/gsap/Observer.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Observer = () => {
+    return (
+        <div>
+
+            <section className="first ">
+                <div className="outer">
+                    <div className="inner">
+                        <div className="four one">
+                            <h2 className="section-heading">Scroll down</h2>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            <section className="second">
+                <div className="outer">
+                    <div className="inner">
+                        <div className="four">
+                            <h2 className="section-heading">Animated with GSAP</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="third">
+                <div className="outer">
+                    <div className="inner">
+                        <div className="four">
+                            <h2 className="section-heading">GreenSock</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="fourth">
+                <div className="outer">
+                    <div className="inner">
+                        <div className="four">
+                            <h2 className="section-heading">Animation platform</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="fifth">
+                <div className="outer">
+                    <div className="inner">
+                        <div className="four">
+                            <h2 className="section-heading">Keep scrolling</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Observer
