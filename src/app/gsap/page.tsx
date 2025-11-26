04d1131f802e7595bb4d6d155181f2c2e497e092
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2025-11-26 18:17:17 +0530

    Added Icon to banner made the section cursor
    Added Icon to banner made the section cursor

--- src/app/gsap/page.tsx
+++ src/app/gsap/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import GradientBorder12 from '../components/gsap/GradientBorder12'
 
 const page = () => {
   return (
-    <div className='justify-center items-center flex h-[90vh]'>
-        404 - Page Not Found
-    </div>
+    <>
+    <GradientBorder12/>
+    </>
+    // <div className='justify-center items-center flex h-[90vh]'>
+    //     404 - Page Not Found
+    // </div>
     // <div className='hidden'>
     //   <AnimatedSections/>
     // </div>
